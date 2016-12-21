020d986f7f5fd46a676b5955539543b780b96111
geoffrey1218 <geoff.s.rich@gmail.com>
2016-12-21 14:12:57 -0800

    Add basic user input - replaces replaceable words with uppercase - will eventually replace them with synonyms

---
+++ main.py
@@ -0,0 +1,7 @@
+import userInput
+
+def main():
+	userInput.take_user_input()
+
+if __name__ == "__main__":
+	main()
---
+++ userInput.py
@@ -0,0 +1,19 @@
+def is_replaceable_word(word):
+	nonreplaceable = ['a','an','the','is']
+	if word in nonreplaceable:
+		return False
+	else:
+		return True
+	
+def replace_with_synonyms(input_list):
+	for i in range(len(input_list)):
+		if is_replaceable_word(input_list[i]):
+			input_list[i] = input_list[i].upper()
+	
+def take_user_input():
+	input_list = input("Enter a sentence to be paraphrased: ").split()
+	replace_with_synonyms(input_list)
+	print(*input_list, sep=' ')
+		
+if __name__ == "__main__":
+	take_user_input()
