fd8c53f9e6a87cebf5702cb93205f1c3735fb2bb
geoffrey1218 <geoff.s.rich@gmail.com>
2016-12-21 17:50:23 -0800

    main fixes - make dictionary global - fix parser import name - call init in main - fix print statement

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import user_input
-import parser
+import synonym_parser
 
 dictionary_filename = "mobythes.aur"
+dictionary = []
 
 def init():
-    dictionary = parser.parse(dictionary_filename)
+    dictionary = synonym_parser.parse(dictionary_filename)
 
 def main():
+    init()
     running = True
     while running:
         original_msg = user_input.take_user_input()
         new_msg = user_input.replace_with_synonyms(original_msg)
-        print(new_msg + '\n')
+        print(new_msg)
         again = input("Another? ").lower()
         running = again == 'y' or again == "yes" or again == "si"
-
-init()
 
 if __name__ == "__main__":
     main()
