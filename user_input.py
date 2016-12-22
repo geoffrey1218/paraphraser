50a97ec1cd441410412efa709dd72a66bf5816d1
geoffrey1218 <geoff.s.rich@gmail.com>
2016-12-21 17:49:28 -0800

    Remove dict arguments and get from main instead - also return output_list in replace_with_synonyms

--- user_input.py
+++ user_input.py
@@ -1,13 +1,14 @@
 import random
+import main
 
 # Gets a synonym for the given word from the given dict
 # input: word - the word you want a synonym for
 #        synonym_dict - dict where key is the root word and value is a list of synonyms
 # output: synonym for the word
-def get_synonym(word, synonym_dict):
+def get_synonym(word):
     # if word is key, i.e. we have synonyms for the word
     if word in main.dictionary:
-        synonyms = synonym_dict[word]
+        synonyms = main.dictionary[word]
         return random.choice(synonyms)
     else:
         return word
@@ -24,10 +25,15 @@ def is_replaceable_word(word):
 # Replaces every word in the input list with its synonym, if one exists
 # input: input_list - list of words to replace
 #        synonym_dict - dict where key is the root word and value is a list of synonyms
-def replace_with_synonyms(input_list, synonym_dict):
+# output: list of words with input replaced by synonyms
+def replace_with_synonyms(input_list):
+    output_list = []
     for i in range(len(input_list)):
         if is_replaceable_word(input_list[i]):
-            input_list[i] = get_synonym(input_list[i], synonym_dict)
+            output_list.append(get_synonym(input_list[i]))
+        else:
+            output_list.append(input_list[i])
+    return output_list
 
 # Prompts the user for a sentence and returns a list of words in the sentence
 # output: list of words in the sentence            
