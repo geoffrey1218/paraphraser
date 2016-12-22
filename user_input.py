dc4f2ad3ba932fe3a034e88b01cc225d5fa83133
geoffrey1218 <geoff.s.rich@gmail.com>
2016-12-21 17:33:24 -0800

    Add get_synonym function - called in replace_with_synonyms

--- user_input.py
+++ user_input.py
@@ -1,3 +1,17 @@
+import random
+
+# Gets a synonym for the given word from the given dict
+# input: word - the word you want a synonym for
+#        synonym_dict - dict where key is the root word and value is a list of synonyms
+# output: synonym for the word
+def get_synonym(word, synonym_dict):
+    # if word is key, i.e. we have synonyms for the word
+    if word in main.dictionary:
+        synonyms = synonym_dict[word]
+        return random.choice(synonyms)
+    else:
+        return word
+
 # Determines if word should be replaced by a synonym
 # output: True if replaceable, False if not
 def is_replaceable_word(word):
@@ -9,10 +23,11 @@ def is_replaceable_word(word):
 
 # Replaces every word in the input list with its synonym, if one exists
 # input: input_list - list of words to replace
-def replace_with_synonyms(input_list):
+#        synonym_dict - dict where key is the root word and value is a list of synonyms
+def replace_with_synonyms(input_list, synonym_dict):
     for i in range(len(input_list)):
         if is_replaceable_word(input_list[i]):
-            input_list[i] = input_list[i].upper()
+            input_list[i] = get_synonym(input_list[i], synonym_dict)
 
 # Prompts the user for a sentence and returns a list of words in the sentence
 # output: list of words in the sentence            
